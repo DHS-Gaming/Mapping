32ab49c10f7dc4298c0e5afca005a7d2b49430d6
Dr. Ignaz M <9mmfullclip@gmail.com>
2016-04-01 15:22:19 +0200

    Autobahn WIP

---
+++ Zivilisten/Autobahn/003_Leitplanken/copy_from_xcam_to_repo.bat
@@ -0,0 +1,7 @@
+@echo off
+
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" _xcamProjects.ini
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_AutobahnLeitplanken.ini" .
+copy "D:\Steam\steamapps\common\Arma 3\mission_Altis_AutobahnLeitplanken.sqm" .
+
+del "D:\Steam\steamapps\common\Arma 3\mission_Altis_AutobahnLeitplanken.sqm"
---
+++ Zivilisten/Autobahn_OLD001/copy_from_xcam_to_repo.bat
@@ -0,0 +1,7 @@
+@echo off
+
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" _xcamProjects.ini
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_Autobahn.ini" Altis_Autobahn.ini
+copy "D:\Steam\steamapps\common\Arma 3\mission_Altis_Autobahn.sqm" mission_Altis_Autobahn.sqm
+
+del "D:\Steam\steamapps\common\Arma 3\mission_Altis_Autobahn.sqm"
