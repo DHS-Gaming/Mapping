356a2ec44ee69329e0e06faa06d8087861d68dd0
Dr. Ignaz M <9mmfullclip@gmail.com>
2016-04-02 02:59:36 +0200

    yo

--- Zivilisten/Autobahn/001_Einschalung/copy_from_xcam_to_repo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" _xcamProjects.ini
-copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_Autobahn2.ini" .
-copy "D:\Steam\steamapps\common\Arma 3\mission_Altis_Autobahn2.sqm" .
-
-del "D:\Steam\steamapps\common\Arma 3\mission_Altis_Autobahn2.sqm"
